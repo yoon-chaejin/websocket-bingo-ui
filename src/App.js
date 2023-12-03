46656bc3c717366ce97b9349ddb2bda183e2ac5f
jaeyoung choi <kevin3st@naver.com>
2023-12-03 14:06:01 +0000

    feat: Client 단에서 Random 한 Player 정보를 생성

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import * as StompJS from '@stomp/stompjs';
 function App() {
   
   const [step, setStep] = useState(0); // 진행 단계를 의미 (0: 접속 전, 1: 빙고판 제작, 2: 게임 진행, 3: 게임 종료)
+  const [player, setPlayer] = useState({empNo : '', name : ''});
 
   const client = useRef(null);
   const subscription = useRef(null);
@@ -19,13 +20,16 @@ function App() {
       console.log("Connected");
       console.log(frame);
 
+      const player = createRandomPlayer();
+      setPlayer(player);
+      registerPlayer(player);
+
       subscription.current = client.current.subscribe('/topic/set-ready', (message) => {
         if (message.body === "true") {
-          alert(" Received Message : Ready " + message.body);
+          alert("Received Message : Ready " + message.body);
           setStep(2);
         }
       });
-
 
       subscription.current = client.current.subscribe('/topic/select-item', (message) => {
         alert(" Received Message : Item " + message.body);
@@ -45,11 +49,29 @@ function App() {
     setStep(1);
   };
 
+  const createRandomPlayer = () => {
+    const empNo = Math.round(Math.random() * 10000).toString();
+    const name = Math.random().toString(36).substring(2, 10);
+
+    console.log("Random Player : " + empNo + " / " + name);
+
+    return {empNo, name};
+  }
+
+  const registerPlayer = (player) => {
+    console.log("Register", player);
+    client.current.publish({
+      destination: '/app/register-player',
+      body: JSON.stringify(player),
+      headers: { 'contentType': 'application/json' }
+    });
+  }
   const sendReady = () => {
-    console.log("Send Ready");
+    console.log("Send Ready", player);
     client.current.publish({
       destination: '/app/set-ready',
-      body: "홍길동"
+      body: JSON.stringify(player),
+      headers: { 'contentType': 'application/json' }
     });
   };
 
